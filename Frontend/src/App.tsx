848029e491bcb682b74e53f317e78b97248060b4
Parth Patel <140066704+Parth002933v@users.noreply.github.com>
2024-08-01 12:39:56 +0530

    Initial commit

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from 'react'
+import Textcomponent from './components/Textcomponent'
+
+function App() {
+  return (
+    <>
+      <Textcomponent />
+    </>
+  )
+}
+
+export default App
